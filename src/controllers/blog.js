f24da423a52aed08c58ab29d48dcecab47fb45a2
unknown <hello.safrian@gmail.com>
2021-10-25 10:04:42 +0700

    validasi request body dan error response dinamis

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,7 +1,19 @@
+const {validationResult} = require('express-validator');
+
 exports.createBlogPost = (req, res, next) => {
     const title = req.body.title;
     // const image = req.body.image;
     const body = req.body.body;
+
+    // ngecek error dari data post yang dikirim
+    const erorrs = validationResult(req);
+
+    if(!erorrs.isEmpty()){
+        const err = new Error('Invalid value');
+        err.errStatus = 400;
+        err.data = erorrs.array();
+        throw err;
+    }
 
     const result = {
         message: "Create Blog Post Success",
