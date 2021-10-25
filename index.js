918ca489827f5e27d9989ebe01b61fa408321600
unknown <hello.safrian@gmail.com>
2021-10-25 16:03:44 +0700

    Upload image with multer

--- index.js
+++ index.js
@@ -3,12 +3,36 @@ const app = express();
 const mongoose = require('mongoose');
 const authRoutes = require('./src/routes/auth');
 const blogRoutes = require('./src/routes/blog');
+const multer = require('multer');
 
-app.use(express.urlencoded({extended: true}));
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().getTime() + '-' + file.originalname)
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());// data yang akan diterima adalah type json
+// middleware
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 
 // mengatasi cors
-app.use((req,res, next) => {
+app.use((req, res, next) => {
     // beberapa ketentuan
     // res.setHeader('Access-Control-Allow-Origin', 'https://codepen.io'); // hanya url spesifik yg bisa mengakses api
     res.setHeader('Access-Control-Allow-Origin', '*'); // untuk bisa diakses semua url dari luar
@@ -39,7 +63,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose.connect('mongodb://rian:Mybp3dtwM729eOiZ@learn-nodejs-shard-00-00.3nsaa.mongodb.net:27017,learn-nodejs-shard-00-01.3nsaa.mongodb.net:27017,learn-nodejs-shard-00-02.3nsaa.mongodb.net:27017/blog?ssl=true&replicaSet=atlas-khjmlp-shard-0&authSource=admin&retryWrites=true&w=majority')
-.then(() => {
-    app.listen(4000, () => console.log('Connection Success!')); // set port
-})
-.catch(err => console.log(err));
+    .then(() => {
+        app.listen(4000, () => console.log('Connection Success!')); // set port
+    })
+    .catch(err => console.log(err));
--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -2,26 +2,35 @@ const {validationResult} = require('express-validator');
 const BlogPost = require('../models/blog');
 
 exports.createBlogPost = (req, res, next) => {
-    const title = req.body.title;
-    // const image = req.body.image;
-    const body = req.body.body;
-
+    
     // ngecek error dari data post yang dikirim
     const erorrs = validationResult(req);
-
+    
     if(!erorrs.isEmpty()){
         const err = new Error('Invalid value');
         err.errStatus = 400;
         err.data = erorrs.array();
         throw err;
     }
+    
+    if(!req.file) {
+        const err = new Error('Image harus diupload');
+        err.errStatus = 422;
+        err.data = erorrs.array();
+        throw err;
+    }
+    
+    const title = req.body.title;
+    const image = req.file.path;
+    const body = req.body.body;
 
     const Posting = new BlogPost({
         title: title,
         body: body,
+        image: image,
         // static author
         author: {
-            id: 1,
+            uid: 1,
             name: 'Rian Cellent'
         }
     });
